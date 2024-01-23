20cbe1600ab1f65d0de6535103b1a04dff5010b9
Molly Sandler <74861421+mollysandler@users.noreply.github.com>
2024-01-23 15:13:05 -0800

    cbc correct and complete, and added result bmps.

--- cbc.py
+++ cbc.py
@@ -1,54 +1,47 @@
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 
-
-##not allowed to use the pad() stuff, gonna need to write our own. 
 def generate_key():
     return get_random_bytes(16)  # 16 bytes for AES-128
 
-def pkcs7_pad(data, block_size):
-    padding_value = block_size - len(data) % block_size
-    padded_data = data + bytes([padding_value] * padding_value)
-    return padded_data
+def pkcs7_pad(data, block_size): #writing my own padding function
+   padding_value = block_size - len(data) % block_size
+   padded_data = data + bytes([padding_value] * padding_value)
+   return padded_data
 
-def cbc_encrypt(plaintext, key, iv):
-    cipher = AES.new(key, AES.MODE_ECB)
+def cbc_encrypt(plaintext, key, iv): 
+    cipher = AES.new(key, AES.MODE_ECB) 
     blocks = [plaintext[i:i+AES.block_size] for i in range(0, len(plaintext), AES.block_size)]
-    ciphertext = b""
+    ciphertext = b"" 
     previous_block = iv
 
     for block in blocks:
-        xored_block = bytes([a ^ b for a, b in zip(block, previous_block)])
+        xored_block = bytes([a ^ b for a, b in zip(block, previous_block)]) #exclusive or function 
         encrypted_block = cipher.encrypt(xored_block)
-        ciphertext += encrypted_block
+        ciphertext += encrypted_block #adding the blocks 
         previous_block = encrypted_block
 
     return ciphertext
 
 def main():
-    ofile = open("./mustang.bmp", "rb")
-    cp_cbc = open("cbc_encrypted.bmp", "wb")  # Open in binary write mode
+    ofile = open("./mustang.bmp", "rb") #mustang getting edited for cbc
+    cp_cbc = open("mustang_cbc.bmp", "wb") 
 
     # Read the entire file content
-    original_text = ofile.read()
+    og = ofile.read()
 
-    # Separate header and data
-    original_header = original_text[:54]
-    data = original_text[54:]
+    original_header = og[:54] #54 byte header as per specs
+    data = og[54:]
 
-    # Padding the data
-    padded_data = pkcs7_pad(data, AES.block_size)
+    padded_data = pkcs7_pad(data, AES.block_size) #pad the data
 
-    # Generate key and IV
-    key = generate_key()
-    iv = get_random_bytes(AES.block_size)
+    key = generate_key() #get the key
+    iv = get_random_bytes(AES.block_size) #should be 128 
 
-    # Encryption using CBC mode
-    encrypted_cbc = cbc_encrypt(padded_data, key, iv)
-    cp_cbc.write(original_header + encrypted_cbc)
-
-    # Close the files
-    ofile.close()
+    encrypted_cbc = cbc_encrypt(padded_data, key, iv) #encrypt based on key
+    cp_cbc.write(original_header + encrypted_cbc) #write everything to the file
+    
+    ofile.close() #close files
     cp_cbc.close()
 
 if __name__ == "__main__":
