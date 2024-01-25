747f8433b105ab5dbb6e891718ef4ba478375e8e
glaurain <94200101+glaurain@users.noreply.github.com>
2024-01-25 13:46:01 -0800

    completed task2.py

--- task2.py
+++ task2.py
@@ -8,26 +8,28 @@ iv = get_random_bytes(16)
 
 
 def pkcs7_pad(data, block_size): #writing my own padding function
-   padding_value = block_size - len(data) % block_size
-   padded_data = data + bytes([padding_value] * padding_value)
+   padding_value = block_size - len(data) % block_size #16 bytes-remaining space
+   padded_data = data + bytes([padding_value] * padding_value) # create bytes object
    return padded_data
 
 def cbc_encrypt(plaintext, key, iv): 
-    cipher = AES.new(key, AES.MODE_CBC, iv)  # Use CBC mode
+    cipher = AES.new(key, AES.MODE_CBC, iv)  # create AES cipher object, Use CBC mode 
     padded_plaintext = pkcs7_pad(plaintext, AES.block_size)  # Pad the plaintext
-    ciphertext = cipher.encrypt(padded_plaintext)
+    ciphertext = cipher.encrypt(padded_plaintext) # encrypt the padded plaintext
     return ciphertext
 
-def decrypt(encryptedParams):
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    paddedParams = cipher.decrypt(encryptedParams)
-    return unpad(paddedParams, AES.block_size, style='pkcs7')
+def decrypt(encryptedText):
+    decipher = AES.new(key, AES.MODE_CBC, iv)
+    paddedText = decipher.decrypt(encryptedText)
+    original = unpad(paddedText, AES.block_size, style='pkcs7')
+    return original
 
 
 def submit():
     userInput = "#admin^true#"
     modifiedInput = "userid=456;userdata=" + userInput + ";session-id=31337"
     print("Modified Input: ", modifiedInput)
+    print("\n")
     
     ciphertext = cbc_encrypt(modifiedInput.encode(), key, iv)
     return ciphertext
@@ -47,10 +49,12 @@ def verify(encryptedInput):
     
     modified = modify(encryptedInput)
     print("modified input: ", modified)
+    print("\n")
 
     decrypted = decrypt(modified)
 
     print("Decrypted: ", decrypted)
+    print("\n")
 
     if b";admin=true;" in decrypted:
         return True
@@ -61,6 +65,7 @@ def verify(encryptedInput):
 def main():
     encrypted = submit()
     print("original: ", encrypted)
+
     print("verification:", verify(encrypted))
 
 
